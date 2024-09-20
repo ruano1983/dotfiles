1dca246026e2990508e6940ee4f14e7636c82158
Ivan Ruano <ruano1983@gmail.com>
2024-09-20 17:27:58 +0200

    color change in qtile

--- qtile/modules/layouts.py
+++ qtile/modules/layouts.py
@@ -1,14 +1,15 @@
 from libqtile import layout
+color="#007984"
 
 layouts = [
     layout.Columns(
-            border_focus=["#b82016", "#b82016"],border_normal=["#111111"],border_width=1,fair=True,insert_position=1,change_size=30,border_on_single=True ),
+            border_focus=[color,color],border_normal=["#111111"],border_width=1,fair=True,insert_position=1,change_size=30,border_on_single=True ),
     layout.MonadTall(
-            border_focus=["#b82016", "#b82016"],border_normal=["#111111"],border_width=1,change_size=30,border_on_single=True ), 
+            border_focus=[color,color],border_normal=["#111111"],border_width=1,change_size=30,border_on_single=True ), 
     layout.MonadWide(
-            border_focus=["#b82016", "#b82016"],border_normal=["#111111"],border_width=1,change_size=30,border_on_single=True ),
+            border_focus=[color,color],border_normal=["#111111"],border_width=1,change_size=30,border_on_single=True ),
     layout.Max(
-            border_focus=["#b82016", "#b82016"],border_normal=["#111111"],border_width=1, border_on_single=True ), 
+            border_focus=[color,color],border_normal=["#111111"],border_width=1, border_on_single=True ), 
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
--- qtile/modules/screens.py
+++ qtile/modules/screens.py
@@ -4,7 +4,7 @@ from .globals import *
 from libqtile.config import Screen
 ## functions
 def fc_separation():
-    return  widget.Spacer(length=10)
+    return  widget.Spacer(length=12)
 def fc_textbox(icon,p=6):
     return widget.TextBox(fontsize=14,padding=p,text=icon)
 def run_btm():
@@ -12,25 +12,26 @@ def run_btm():
 def run_vol():
     qtile.cmd_spawn('pavucontrol')
 def init_colors():
-    return[["#b82016","#b82016"], # color1
-           ["#0d62ac","#0d62ac"]] # color2
+    return[["#b82016","#b82016"], # orange
+           ["#0d62ac","#0d62ac"], # blue
+           ["#007984","#007984"]] # green
 colors = init_colors()
 screens = [
     Screen(
         bottom=bar.Bar([
                 widget.TextBox(
-                    fontsize=16,text='',background=colors[0],padding=18,
+                    text='',background=colors[2],padding=14,
                     mouse_callbacks={'Button1':lambda:qtile.cmd_spawn(rofi)}),
                 widget.GroupBox(highlight_method='block', block_highlight_text_color="#ffffff",
-                    highlight_color=[colors[0],colors[0]],  this_current_screen_border="#b82016",
+                    highlight_color=[colors[2],colors[2]],  this_current_screen_border=colors[2],
                     inactive="#484848", padding=8 ,rounded=False, borderwidth=4 ),
-                widget.CurrentLayout(background=colors[0],padding=8),
+                widget.CurrentLayout(background=colors[2],padding=8),
                 widget.Prompt(prompt="Run "),
                 widget.Spacer(length=8),
                 fc_textbox(icon='󰖯',p=3),        
                 widget.WindowName(max_chars=50),
                 fc_textbox(icon='󰧈'),
-                widget.Net(format='{down:.0f}{down_suffix} ↓↑ {up:.0f}{up_suffix}',update_interval=2),
+                widget.Net(format='{down:.0f}{up_suffix} ↑↓ {up:.0f}{down_suffix}',update_interval=2),
                 fc_separation(),
                 fc_textbox(icon='󰖩'),
                 widget.Wlan(format='{percent:2.0%}'),
@@ -53,7 +54,8 @@ screens = [
                 fc_textbox(icon='󰌌'),
                 widget.KeyboardLayout(configured_keyboards=['us altgr-intl','es'],display_map={ 'us altgr-intl': 'ansi', 'es':'es'}),
                 fc_separation(),
-                widget.OpenWeather(app_key="bb789b9c68ed3ee12c7f8d99d62f3c3b",location='Fuenlabrada', format='{icon} {weather_details} {main_temp:.0f}°{units_temperature}'),
+                fc_textbox(icon=''),
+                widget.OpenWeather(app_key="bb789b9c68ed3ee12c7f8d99d62f3c3b",location='Fuenlabrada', format='{weather} {main_temp:.0f}°{units_temperature}'),
                 fc_separation(),
                 fc_textbox(icon='󰥔'),
                 widget.Clock(format="%A %d %b %H:%M"),
