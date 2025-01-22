be55060a32baf98087a7e6334949fcb56f457b7b
Ivan Ruano <ruano1983@gmail.com>
2025-01-22 23:34:47 +0100

    updated autostart in qtile

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,7 +3,7 @@ kanshi &
 dbus-update-activation-environment --all &
 /usr/libexec/pipewire-launch.sh &
 /usr/libexec/polkit-kde-authentication-agent-1 &
-/usr/bin/dunst -config ~/.config/dunst/dunstrc &
+/usr/bin/dunst -config ~/.config/dunst/dunstrc-qtile &
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita' &
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &
 gsettings set org.gnome.desktop.interface font-name 'Noto Sans 8' &
