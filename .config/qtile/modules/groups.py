370eedd3146f4897685e4837b45586211f425c2b
Ivan Ruano <ruano1983@gmail.com>
2025-05-05 00:49:23 +0200

    Removed comments in the groups module of qtile

--- .config/qtile/modules/groups.py
+++ .config/qtile/modules/groups.py
@@ -18,23 +18,17 @@ groups = [
 for i in groups:
     keys.extend(
         [
-            # mod1 + group number = switch to group
             Key(
                 [mod],
                 i.name,
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
-            # mod1 + shift + group number = switch to & move focused window to group
             Key(
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
                 desc="Switch to & move focused window to group {}".format(i.name),
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + group number = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
