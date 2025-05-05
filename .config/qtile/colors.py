dffce8ca752e5d00305c928d2163cbea40b4f183
Ivan Ruano <ruano1983@gmail.com>
2025-05-06 00:08:04 +0200

    changes in color comments

--- .config/qtile/colors.py
+++ .config/qtile/colors.py
@@ -1,143 +1,143 @@
 Argyls = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#2fa6c7" , "#2fa6c7"], # PRIMARY BLUE
-    ["#dfdfdf" , "#dfdfdf"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg 
+    ["#2fa6c7" , "#2fa6c7"], # blue
+    ["#dfdfdf" , "#dfdfdf"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 Nube = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#2e86c1" , "#2e86c1"], # PRIMARY BLUE
-    ["#dfdfdf" , "#dfdfdf"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#2e86c1" , "#2e86c1"], # blue
+    ["#dfdfdf" , "#dfdfdf"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 Blue = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#0288d1" , "#0288d1"], # PRIMARY BLUE
-    ["#dfdfdf" , "#dfdfdf"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#0288d1" , "#0288d1"], # blue
+    ["#dfdfdf" , "#dfdfdf"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
       
 Morad = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#673ab7" , "#673ab7"], # PRIMARY BLUE
-    ["#dfdfdf" , "#dfdfdf"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#673ab7" , "#673ab7"], # blue
+    ["#dfdfdf" , "#dfdfdf"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 Purple = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#663399" , "#663399"], # PRIMARY BLUE
-    ["#dfdfdf" , "#dfdfdf"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#663399" , "#663399"], # blue
+    ["#dfdfdf" , "#dfdfdf"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
     
  
 DarkOrange = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#f57c00" , "#f57c00"], # PRIMARY ORANGE
-    ["#dfdfdf" , "#dfdfdf"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#f57c00" , "#f57c00"], # orange
+    ["#dfdfdf" , "#dfdfdf"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 Ataru = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#ef6c00" , "#ef6c00"], # PRIMARY ORANGE
-    ["#dfdfdf" , "#dfdfdf"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#ef6c00" , "#ef6c00"], # orange
+    ["#dfdfdf" , "#dfdfdf"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 Lune = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#0c956c" , "#0c956c"], # PRIMARY GREEN
-    ["#dfdfdf" , "#dfdfdf"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#0c956c" , "#0c956c"], # green
+    ["#dfdfdf" , "#dfdfdf"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 
 Thaxi = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#ffc022" , "#ffc022"], # PRIMARY YELLOW
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"],  # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # BLACK
+    ["#121212" , "#121212"], # bg
+    ["#ffc022" , "#ffc022"], # yellow
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 Amor = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#e91e63" , "#e91e63"], # PRIMARY RED
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#e91e63" , "#e91e63"], # red
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 Lime = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#cddc39" , "#cddc39"], # PRIMARY YELLOW
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#cddc39" , "#cddc39"], # yellow
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
     
 Yellow = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#ffc107" , "#ffc107"], # PRIMARY YELLOW
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#ffc107" , "#ffc107"], # yellow
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 Olive = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#6B8E23" , "#6B8E23"], # PRIMARY GREEN
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#6B8E23" , "#6B8E23"], # green
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
     
 OliveDark = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#556B2F" , "#556B2F"], # PRIMARY GREEN
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#556B2F" , "#556B2F"], # green
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
     
 Forest = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#827717" , "#827717"], # PRIMARY GREEN
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#827717" , "#827717"], # green
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 Aquamarine = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#20B2AA" , "#20B2AA"], # PRIMARY GREEN
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#20B2AA" , "#20B2AA"], # green
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 
 AquamarineDark = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#008B8B" , "#008B8B"], # PRIMARY GREEN
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#008B8B" , "#008B8B"], # green
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
 BlueGray = [
-    ["#121212" , "#121212"], # BG BLACK
-    ["#607d8b" , "#607d8b"], # PRIMARY BLUE
-    ["#f8f8f2" , "#f8f8f2"], # FG WHITE
-    ["#383838" , "#383838"], # BLACK
-    ["#F8F8F2" , "#F8F8F2"]  # WHITE
+    ["#121212" , "#121212"], # bg
+    ["#607d8b" , "#607d8b"], # blue
+    ["#f8f8f2" , "#f8f8f2"], # fg
+    ["#383838" , "#383838"], # black
+    ["#F8F8F2" , "#F8F8F2"]  # white
     ]
