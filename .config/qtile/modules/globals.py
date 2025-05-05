08290c0fc0d70b033630c53d8f9984ff79d69214
Ivan Ruano <ruano1983@gmail.com>
2025-05-05 23:41:45 +0200

    add tuple in globals module

--- .config/qtile/modules/globals.py
+++ .config/qtile/modules/globals.py
@@ -2,8 +2,7 @@ import os
 mod = "mod4"
 myTerm = "alacritty"
 myBrowser = "LibreWolf.x86_64.AppImage"
-myfm = "vifm"
-myfm2 = "ranger"
+myfm = ("thunar" , "ranger", "vifm")
 myChat = "session-desktop.AppImage"
 wallpaper = os.path.join(os.path.expanduser('~/Imágenes/wallpapers/'), 'arquitectura/508887.jpg')
 rofi = os.path.expanduser('~/.config/rofi/launchers/type-2/launcher.sh')
