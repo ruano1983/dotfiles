a2ca3a8c040dde99c1725cbf3c37da841c63d01f
Ivan Ruano <ruano1983@gmail.com>
2025-05-06 23:59:24 +0200

    changes in autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+COLORSCHEME=argyls
 kanshi &
 dbus-update-activation-environment --all &
-/usr/libexec/pipewire-launch.sh &
-/usr/libexec/polkit-kde-authentication-agent-1 &
-/usr/bin/dunst -config ~/.config/dunst/dunstrc &
+/usr/libexec/polkit-gnome-authentication-agent-1 &
+
+if [ -f /usr/libexec/pipewire-launch.sh  ]; then
+    /usr/libexec/pipewire-launch.sh &
+fi
+
+if [ -d ~/.config/dunst ]; then
+    /usr/bin/dunst -config ~/.config/dunst/dunstrc-$COLORSCHEME &
+fi
+
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita' &
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus' &
 gsettings set org.gnome.desktop.interface font-name 'Noto Sans 8' &
