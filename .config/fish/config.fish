5bf76cefa0f955268bed7330b99bf5ea6fb36147
Ivan Ruano <ruano1983@gmail.com>
2025-05-11 23:52:33 +0200

    changes in alias the fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,8 @@ set -g fish_prompt_pwd_dir_length 80
 set -e fish_user_paths
 set -U fish_user_paths  $HOME/.local/bin  $HOME/Applications  $HOME/Aplicaciones/Telegram $HOME/scripts  $fish_user_paths
 set fish_greeting	 # greeting	
+set TERM "xterm-256color"	 # Sets the terminal type
+set EDITOR "vim"		 # $EDITOR use vim
 # radiotunes
 alias tuneslatino="mpv --no-video 'http://prem2.radiotunes.com:80/latinpophits?99fc5648cc8e87d633d28b0b'"
 alias tunesdance="mpv --no-video 'http://prem2.radiotunes.com:80/dancehits?99fc5648cc8e87d633d28b0b'"
@@ -22,15 +24,14 @@ alias play24h="mpv 'https://ztnr.rtve.es/ztnr/1694255.m3u8'"
 alias playgol="mpv 'https://spa-ha-p004.cdn.masmediatv.es/SVoriginOperatorEdge/smil:5_HD.smil/manifest.m3u8'"
 alias playla1="mpv 'https://ztnr.rtve.es/ztnr/1688877.m3u8'"
 alias playmadridtv="mpv 'https://telemadrid-23-secure2.akamaized.net/master.m3u8'"
-# tar xvf
+# tar (compresion)
 alias tarx="tar xvf"
+alias tarc="tar cvJf" 
 # mlocate updatedb
 alias updb="updatedb  --prunepaths=/run"
-# qtile wayland start
+# window manager start
 alias qtilestart="dbus-run-session qtile start -b wayland"
-# dwl wayland start
 alias dwlstart="dbus-run-session dwl -s waybar"
-# sway wayland start
 alias swaystart="dbus-run-session sway"
 # plasma wayland start
 alias kde="dbus-run-session startplasma-wayland"
@@ -42,12 +43,19 @@ alias grep='grep --color=auto'
 alias vifmp="vifm /build/ports /build/ports"
 alias vifmc="vifm ~/.config ~/.config"
 # ranger
-alias rangerv="ranger --cmd='set viewmode=multipane' ~/ ~/"
+alias rangerh="ranger --cmd='set viewmode=multipane' ~/ ~/"
 alias rangerp="ranger  /build/ports"
 # eza 
 alias eza="eza -G --group-directories-first"
 # Changing "ls" to "eza"
-alias ls="eza -G --group-directories-first"
+alias ls='eza -G --color=always --group-directories-first' # my preferred listing
+alias la='eza -a --color=always --group-directories-first'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first'  # long format
+alias lt='eza -aT --color=always --group-directories-first' # tree listing
+alias l.='eza -al --color=always --group-directories-first ../' # ls on the PARENT directory
+alias l..='eza -al --color=always --group-directories-first ../../' # ls on directory 2 levels up
+alias l...='eza -al --color=always --group-directories-first ../../../' # ls on directory 3 levels up
+#alias ls="eza -G --group-directories-first"
 # mpv play
 alias play="mpv --shuffle --no-video"
 # wl-randr
@@ -73,21 +81,26 @@ alias trim="sudo fstrim -a"
 # smart nvme
 alias smartnvme1="sudo smartctl -x /dev/nvme0n1"
 alias smartnvme2="sudo smartctl -x /dev/nvme1n1"
+# download LibreWolf
+alias lwolfdown="fish ~/scripts/download_librewolf.fish ~/Aplicaciones"
 # install firefox script
 alias mozdown="sudo python3 ~/scripts/python/mozilla-download.py"
 # rename files 
 alias renove="python3 ~/scripts/python/rename.py"
-# build linux script
+# clean ports (.md5sum .footprint)
+alias cleanports="fish ~/scripts/clean_ports.fish"
+# build apps script
 alias buildlinux="fish /home/ivanruano83/scripts/build_linux.fish -f /linux -i vmlinuz-frikios"
-# build qtile script
 alias buildqtile="fish ~/scripts/build_qtile.fish -f qtile_env -b wayland"
-# build darktable script
 alias builddarktable="fish ~/scripts/build_darktable.fish"
+alias buildkde="fish ~/scripts/build_kde.fish"
 # git
-alias gl="git pull"
-alias ga="git add ."
-alias gc="git commit -m"
-alias gp="git push"
+alias pull="git pull"
+alias addall="git add ."
+alias add="git add"
+alias commit="git commit -m"
+alias push="git push"
+alias clone='git clone'
 # prt-get
 alias prtin="prt-get install"
 alias prtup="prt-get update -fr"
@@ -99,8 +112,6 @@ alias prtrem="prt-get remove"
 alias makepkg="fakeroot pkgmk -d"
 alias pkgin="pkgadd"
 alias pkgup="pkgadd -u"
-set TERM "xterm-256color"	 # Sets the terminal type
-set EDITOR "vim"		 # $EDITOR use vim
 if [ (tty) = "/dev/tty1" ]
     #exec dbus-run-session qtile start -b wayland
     #exec dbus-run-session startplasma-wayland
