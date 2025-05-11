3fe58cc55c8bfa149063bae917527c98cdb599e4
Ivan Ruano <ruano1983@gmail.com>
2025-05-11 14:53:57 +0200

    changed order in the layout

--- .config/qtile/modules/layouts.py
+++ .config/qtile/modules/layouts.py
@@ -6,20 +6,18 @@ layout_theme = {"border_width": 1,
                 "border_normal": colors[3],
                 "border_on_single":True 
                 }
-
 layouts = [
+    layout.Tile(
+            **layout_theme,ratio=0.55), 
     layout.Max(
             **layout_theme), 
     layout.Columns(
             **layout_theme,fair=True,insert_position=1),
-    layout.Tile(
-            **layout_theme,ratio=0.55),  
-    layout.MonadTall(
+        layout.MonadTall(
             **layout_theme), 
     layout.MonadWide(
             **layout_theme),
-    
-    # layout.Stack(num_stacks=2),
+       # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
     # layout.MonadTall(),
