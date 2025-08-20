5819a47a8ca9f273e3f5fc8b3f45a291fcf0f2c1
Ivan Ruano <ruano1983@gmail.com>
2025-08-20 15:03:22 +0200

    add autostart script

---
+++ autostart.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+COLORSCHEME=waybar
+kanshi &
+dbus-update-activation-environment --all &
+/usr/libexec/polkit-kde-authentication-agent-1 &
+
+if [ -f /usr/libexec/pipewire-launch.sh  ]; then
+    /usr/libexec/pipewire-launch.sh &
+fi
+
+if [ -d ~/.config/dunst ]; then
+    /usr/bin/dunst -config ~/.config/dunst/dunstrc-$COLORSCHEME &
+fi
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita' &
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus' &
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 8' &
+
+sleep 1
+
+if [ -f /usr/local/bin/pa-notify  ]; then
+    /usr/local/bin/pa-notify &
+fi
+
+
